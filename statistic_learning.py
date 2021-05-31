8d544dd5578261d912b243ddbc2080c3ae380933
smogork <krzysztof.smogor@wp.pl>
2021-05-31 23:46:58 +0200

    Promowanie dlugich slow oraz logarytm na wystapien

--- statistic_learning.py
+++ statistic_learning.py
@@ -2,6 +2,7 @@
 
 import sys
 import csv
+from math import log
 
 class TranslationRecord:
     def __init__(self, translation):
@@ -107,9 +108,9 @@ def select_best_translations(translation_dictionary, pol_count, pol_words_total)
         best_pair = ("", "")
 
         for translation in record.original_words:
-            target_probability = pol_count[translation] / pol_words_total
+            target_probability = log(pol_count[translation]) / pol_words_total
             translation_probability = record.original_words[translation] / record.total_original_words
-            prob = target_probability * translation_probability
+            prob = target_probability * translation_probability * len (translation)
             if prob > best_probability:
                 best_probability = prob
                 best_pair = (record.translation, translation)
